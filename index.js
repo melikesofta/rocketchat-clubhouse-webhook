dd8398304222a2807d2cc050fdecf72c43cb80b2
Meric Melike Softa <melike.softa@envanterium.com>
2019-09-26 13:39:15 +0300

    Improve text & links for various cases

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ class Script {
       const channel = url.query.channel;
       const { member_id, actions } = content;
       const userName = MEMBER_NAMES_BY_UUID[member_id] || member_id;
+      const userTag = userName ? `@${userName}` : "unknown user";
 
-      let result = this.determineResultByActions(actions, userName);
+      let result = Script.determineResultByActions(actions, userTag);
 
       if (result && result.content && channel) {
         result.content.channel = '#' + channel;
@@ -31,9 +32,14 @@ class Script {
     }
   }
 
-  determineResultByActions(actions, userName) {
+  static determineResultByActions(actions, userTag) {
     const action = actions[0];
-    const text = this.generateTextByAction(action, userName);
+    const text = Script.generateTextByAction(action, userTag);
+    let slug = action.entity_type;
+
+    if ("story-comment" === slug || "story-task" === slug) {
+      slug = "story";
+    }
 
     return {
       content: {
@@ -42,7 +48,7 @@ class Script {
         attachments: [
           {
             title: `${action.name} [${action.id}]`,
-            title_link: `${BASE_URL}/${action.entity_type}/${action.id}`,
+            title_link: `${BASE_URL}/${slug}/${action.id}`,
             text: "",
             image_url: "",
             color: "#764FA5"
@@ -52,37 +58,15 @@ class Script {
     };
   }
 
-  generateTextByAction(action, userName) {
-    let text = `${action.action} action by ${userName ? `@${userName}` : "unknown user"} on ${action.entity_type} "${action.name}".`;
-
-    text = `${text}\n\nChanges:\n`;
-
-    for (let changeKey in action.changes) {
-      const change = action.changes[changeKey];
-
-      if (!change) {
-        return "Parse Error";
-      }
-
-      if (IGNORED_KEYS.includes(changeKey)) {
-        // Do nothing.
-      } else if (change.hasOwnProperty("old") || change.hasOwnProperty("new")) {
-        if (change.old === change.new) {
-          // Some values are sent in the response by default, like `completed_at from 0 to 0` when it's set to `done`.
-          // Here we ignore the values that didn't change.
-        } else {
-          text += `- *${changeKey}* from *${change.old}* to *${change.new}*\n`;
-        }
-      } else if (change.adds) {
-        text += `- added *${changeKey}* ${this.determineChangeValueByChangeKey(change.adds, changeKey)}\n`
-      } else if (change.removes) {
-        text += `- removed *${changeKey}* ${this.determineChangeValueByChangeKey(change.removes, changeKey)}\n`
-      } else {
-        text += `- Key(s) not implemented: ${Object.keys(change)}\n`;
-      }
+  static generateTextByAction(action, userTag) {
+    switch(action.action) {
+      case "create":
+        return Script.generateTextCreateAction(action, userTag);
+      case "update":
+        return Script.generateTextUpdateAction(action, userTag);
+      default:
+        return Script.generateTextDefault(action, userTag);
     }
-
-    return text;
   }
 
   determineChangeValueByChangeKey(changedValues, changeKey) {
@@ -97,6 +81,69 @@ class Script {
     } else {
       return changedValues.join("")
     }
+  }
+
+  static generateTextCreateAction(action, userTag) {
+    switch(action.entity_type) {
+      case "story":
+        return `${userTag} created a story: "${action.name}"`;
+      case "story-comment":
+        return `${userTag} commented on the story: ${action.name}`;
+      default:
+        return `${userTag} created a ${action.entity_type}: "${action.name}".`;
+    }
+  }
+
+  static generateTextUpdateAction(action, userTag) {
+    /* Special Cases 1: Story Completed */
+    if (
+      action.entity_type === "story" &&
+      action.changes &&
+      action.changes.hasOwnProperty("completed") &&
+      action.changes.completed.old === false &&
+      action.changes.completed.new === true
+    ) {
+      return `${userTag} completed the story "${action.name}" 🙌`;
+    }
+
+    /* Default Case */
+    let text = `${action.action} action by ${userTag} on ${action.entity_type} "${action.name}".`;
+
+    // List changes if any
+    if (action.changes) {
+      text = `${text}\n\nChanges:\n`;
+
+      for (let changeKey in action.changes) {
+        const change = action.changes[changeKey];
+
+        if (!change) {
+          return "Parse Error";
+        }
+
+        if (IGNORED_KEYS.includes(changeKey)) {
+          // Do nothing.
+        } else if (change.hasOwnProperty("old") || change.hasOwnProperty("new")) {
+          if (change.old === change.new) {
+            // Some values are sent in the response by default, like `completed_at from 0 to 0` when it's set to `done`.
+            // Here we ignore the values that didn't change.
+          } else {
+            text += `- *${changeKey}* from *${change.old}* to *${change.new}*\n`;
+          }
+        } else if (change.adds) {
+          text += `- added *${changeKey}* ${this.determineChangeValueByChangeKey(change.adds, changeKey)}\n`
+        } else if (change.removes) {
+          text += `- removed *${changeKey}* ${this.determineChangeValueByChangeKey(change.removes, changeKey)}\n`
+        } else {
+          text += `- Key(s) not implemented: ${Object.keys(change)}\n`;
+        }
+      }
+    }
+
+    return text;
+  }
+
+  static generateTextDefault(action, userTag) {
+    return `${action.action} action by ${userTag} on ${action.entity_type} "${action.name}".`;
   }
 
   static getErrorMessage(error) {
